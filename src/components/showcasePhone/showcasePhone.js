afb6a8dd5e7559949cfb5a04fe1a537b1ba856d7
zlerp <zhpdeals@gmail.com>
2020-04-06 23:19:01 -0400

    first commit

---
+++ src/components/showcasePhone/showcasePhone.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function ShowcasePhone(props) {
+    return (
+        <div>
+            <h1>iPhone</h1>
+        </div>
+    );
+}
---
+++ src/routes.js
@@ -0,0 +1,181 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useHistory,
+    useLocation
+} from "react-router-dom";
+
+import Registration from './components/registration/registration';
+import ShowcasePhone from "./components/showcasePhone/showcasePhone";
+import Login from "./components/login/login";
+
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
+
+export default function Routes() {
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/register">Register</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/iphone">iphone</Link>
+                    </li>
+                </ul>
+
+                <hr />
+
+                {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/register">
+                        <Registration />
+                    </Route>
+                    <Route path="/login">
+                        <Login/>
+                    </Route>
+                    <PrivateRoute path="/iphone">
+                        <ShowcasePhone />
+                    </PrivateRoute>
+                    {/*<Route path="/iphone">*/}
+                        {/*<ShowcasePhone />*/}
+                    {/*</Route>*/}
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    );
+}
+
+function About() {
+    return (
+        <div>
+            <h2>About</h2>
+        </div>
+    );
+}
+
+function Dashboard() {
+    return (
+        <div>
+            <h2>Dashboard</h2>
+        </div>
+    );
+}
+
+
+const fakeAuth = {
+    isAuthenticated: false,
+    authenticate(cb) {
+        fakeAuth.isAuthenticated = true;
+        setTimeout(cb, 100); // fake async
+    },
+    signout(cb) {
+        fakeAuth.isAuthenticated = false;
+        setTimeout(cb, 100);
+    }
+};
+
+function AuthButton() {
+    let history = useHistory();
+
+    return fakeAuth.isAuthenticated ? (
+        <p>
+            Welcome!{" "}
+            <button
+                onClick={() => {
+                    fakeAuth.signout(() => history.push("/"));
+                }}
+            >
+                Sign out
+            </button>
+        </p>
+    ) : (
+        <p>You are not logged in.</p>
+    );
+}
+
+// A wrapper for <Route> that redirects to the login
+// screen if you're not yet authenticated.
+function PrivateRoute({ children, ...rest }) {
+    return (
+        <Route
+            {...rest}
+            render={({ location }) =>
+                fakeAuth.isAuthenticated ? (
+                    children
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/login",
+                            state: { from: location }
+                        }}
+                    />
+                )
+            }
+        />
+    );
+}
+
+function PublicPage() {
+    return <h3>Public</h3>;
+}
+
+function ProtectedPage() {
+    return <h3>Protected</h3>;
+}
+
+function LoginPage() {
+    let history = useHistory();
+    let location = useLocation();
+
+    let { from } = location.state || { from: { pathname: "/" } };
+    let login = () => {
+        fakeAuth.authenticate(() => {
+            history.replace(from);
+        });
+    };
+
+    return (
+        <div>
+            <p>You must log in to view the page at {from.pathname}</p>
+            <button onClick={login}>Log in</button>
+        </div>
+    );
+}
