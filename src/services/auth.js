b4a28b0d1b0cd5c6d2fbf8cf98718ccdd307704c
Zach Perlman <zperlman@ncl.com>
2020-04-07 20:39:22 -0400

    add toLowerCase on email

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,6 @@
 
 export function checkAuth(users, userInfo) {
-    const user = users.find(user => user.email === userInfo.email);
+    const user = users.find(user => user.email.toLowerCase() === userInfo.email.toLowerCase());
     if (user) {
         if (user.password === userInfo.password) {
             return true;
